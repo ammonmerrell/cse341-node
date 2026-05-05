79eba085ef46ed8b1823e1ec665d58457222194b
ammonmerrell <ammonmerrell@outlook.com>
2026-05-05 10:21:39 -0600

    added route instuctions

---
+++ routes/contacts.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router= express.Router();
+
+const contactsControllers = require('../controllers/contacts.js');
+
+router.get('/', contactsControllers.getAll);
+
+router.get('/:id', contactsControllers.getAll);
+
+module.exports = router;
