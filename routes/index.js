d14351ba3ed2ad7896cd66c4f54aa3c8dc830a47
ammonmerrell <ammonmerrell@outlook.com>
2026-05-05 10:21:19 -0600

    added route for contacts

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-const { Router } = require("express");
 
 const router = require("express").Router();
 
-router.get('/', (req, res) =>{res.send('Hello World')});
+router.get('/', (req, res) =>{res.send('Hello World');});
+
+router.get('/contacts', require('./contacts'));
 
 module.exports = router;
