41461313715c6413b303f46a56c7b5bf75f1997f
ammonmerrell <ammonmerrell@outlook.com>
2026-05-06 16:43:56 -0600

    redirected route to contact by using .get

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 
 const router = require("express").Router();
 
-const contactsControllers = require('../controllers/contacts.js');
+
 
 router.get('/', (req, res) =>{res.send('Hello World');});
 
-// router.get('/contacts', require('./contacts'));
-router.get('/contacts',  contactsControllers.getAll);
-router.get('/contacts/:id', contactsControllers.getSingle);
+router.use('/contacts', require('./contacts'));
+
 
 
 module.exports = router;
