ad1095b05dbbc9efa6b46c8383e1582604d2ba06
ammonmerrell <ammonmerrell@outlook.com>
2026-05-06 08:52:54 -0600

    I bypassed contacts route, by commening it out, and putting contact route info into index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 
 const router = require("express").Router();
 
+const contactsControllers = require('../controllers/contacts.js');
+
 router.get('/', (req, res) =>{res.send('Hello World');});
 
-router.get('/contacts', require('./contacts'));
+// router.get('/contacts', require('./contacts'));
+router.get('/contacts',  contactsControllers.getAll);
+router.get('/contacts/:id', contactsControllers.getSingle);
+
 
 module.exports = router;
