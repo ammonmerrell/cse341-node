c156d2fd1542dedc78bd19a82d200a4e44f84907
ammonmerrell <ammonmerrell@outlook.com>
2026-05-11 12:23:31 -0600

    added comment above port disabling callout to process.env.PORT

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongodb = require('./data/database');
 const app = express();
 
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'))
