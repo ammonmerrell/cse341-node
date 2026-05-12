9721f1a7cea24e6faf581ece1ac79fe9f7788c48
ammonmerrell <ammonmerrell@outlook.com>
2026-05-12 10:18:50 -0600

    added requirement to start server funtion on "node swagger.js"

--- swagger.js
+++ swagger.js
@@ -43,4 +43,6 @@ const routes = ['./routes/index.js', './routes/contacts.js'];
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
-swaggerAutogen(outputFile, routes, doc);
+swaggerAutogen(outputFile, routes, doc).then(() => {
+require('./server.js'); // Start your app after docs are built
+});
