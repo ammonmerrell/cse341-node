96ba4a12380f7ab57394aba051d8fa649cb4f31b
ammonmerrell <ammonmerrell@outlook.com>
2026-05-15 12:08:57 -0600

    changed swagger page

--- swagger.js
+++ swagger.js
@@ -1,48 +1,63 @@
 
-const swaggerAutogen = require('swagger-autogen')({openapi: '3.0.0'});
+// const swaggerAutogen = require('swagger-autogen')({openapi: '3.0.0'});
+
+// const doc = {
+//   info: {
+//     version: "1.0.0",
+//     title: 'My API',
+//     description: 'This is the Description'
+//   },
+//   servers: [
+
+//     {
+//         url: '',
+//         description: ''
+//     },
+// ],
+// tags: [                   // by default: empty Array
+//     {
+//       name: '',             // Tag name
+//       description: ''       // Tag description
+//     },
+// ],
+// components: {components: {
+//         schemas: {
+//             contacts: {
+//                 _id: '69ac5e5c914b79cfcfe8dca9',
+//                 firstName: 'Ben',
+//                 lastName: 'Fielding',
+//                 email: 'ben@google.gmail',
+//                 favoriteColor: 'red',
+//                 birthday: '04/2/2002',
+
+//             },
+//         }
+//     }}
+
+// };
+
+
+// const outputFile = './swagger-output.json';
+// const routes = ['./routes/index.js', './routes/contacts.js'];
+
+// /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+// root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+// swaggerAutogen(outputFile, routes, doc).then(() => {
+// require('./server.js'); // Start your app after docs are built
+// });
+
+const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
-  info: {
-    version: "1.0.0",
-    title: 'My API',
-    description: 'This is the Description'
-  },
-  servers: [
-
-    {
-        url: '',
-        description: ''
+    info: {
+        title: "Contacts Api",
+        description: "This api stores contact info.",
     },
-],
-tags: [                   // by default: empty Array
-    {
-      name: '',             // Tag name
-      description: ''       // Tag description
-    },
-],
-components: {components: {
-        schemas: {
-            contacts: {
-                _id: '69ac5e5c914b79cfcfe8dca9',
-                firstName: 'Ben',
-                lastName: 'Fielding',
-                email: 'ben@google.gmail',
-                favoriteColor: 'red',
-                birthday: '04/2/2002',
-
-            },
-        }
-    }}
-
+    host: 'localhost:3000',
+    schemes: ['http', 'https']
 };
+const outputFile ='./swagger.json';
+const endpointsFiles = ['./routes/index.js'];
 
-
-const outputFile = './swagger-output.json';
-const routes = ['./routes/index.js', './routes/contacts.js'];
-
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
-
-swaggerAutogen(outputFile, routes, doc).then(() => {
-require('./server.js'); // Start your app after docs are built
-});
+swaggerAutogen(outputFile, endpointsFiles, doc)
