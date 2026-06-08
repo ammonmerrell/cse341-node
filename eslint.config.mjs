efd516530b1d9c3e818baa72849d8b07caa2c302
ammonmerrell <ammonmerrell@outlook.com>
2026-06-08 13:44:36 -0600

    added jest and related dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
+import { test, expect } from '@jest/globals';
+
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
